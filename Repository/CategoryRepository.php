68e82f9859eb005ac15e49c744e2370d91a76f20
Dylan Schreier <dylan.schreier83@gmail.com>
2018-07-11 17:00:33 +0200

    Add entity (Category)

---
+++ Repository/CategoryRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\CMSBundle\Repository;
+
+use App\CMSBundle\Entity\Category;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Category|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Category|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Category[]    findAll()
+ * @method Category[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CategoryRepository extends ServiceEntityRepository {
+	public function __construct(RegistryInterface $registry) {
+		parent::__construct($registry, Category::class);
+	}
+
+//    /**
+//     * @return Command[] Returns an array of Command objects
+//     */
+	/*
+	public function findByExampleField($value)
+	{
+		return $this->createQueryBuilder('c')
+			->andWhere('c.exampleField = :val')
+			->setParameter('val', $value)
+			->orderBy('c.id', 'ASC')
+			->setMaxResults(10)
+			->getQuery()
+			->getResult()
+		;
+	}
+	*/
+
+	/*
+	public function findOneBySomeField($value): ?Command
+	{
+		return $this->createQueryBuilder('c')
+			->andWhere('c.exampleField = :val')
+			->setParameter('val', $value)
+			->getQuery()
+			->getOneOrNullResult()
+		;
+	}
+	*/
+}
